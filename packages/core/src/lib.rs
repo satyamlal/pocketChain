f569e4bbd561c24fdecfb77ca3ed6b65c8e9517e
Test <ssatyamlal@gmail.com>
2026-05-25 15:27:45 +0530

    add generate_mnemonic_phrase() function to generate 12 seed phrase mnemonic for solana web  wallet

--- packages/core/src/lib.rs
+++ packages/core/src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use bip39::Mnemonic;
+use wasm_bindgen::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn generate_mnemonic_phrase() -> String {
+    let mnenomic = Mnemonic::generate(12).expect("Failed to generate mnemonic!");
+    mnenomic.to_string()
 }
